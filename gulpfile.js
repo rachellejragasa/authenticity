44559d8182ec2c6ad56e1a969ba04c8e2bd2bff1
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-06-17 13:05:14 +0100

    Adding basic sass files and sass lint

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync');
+const sassLint = require('gulp-sass-lint');
 
 gulp.task('sass', function () {
     return gulp.src('assets/css/main.scss')
@@ -14,7 +15,7 @@ gulp.task('sass', function () {
         }))
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function () {
+gulp.task('watch', ['browserSync', 'sass', 'sass-lint'], function () {
     gulp.watch('assets/css/**/*.scss', ['sass']);
 
 })
@@ -28,4 +29,14 @@ gulp.task('browserSync', function() {
     browserSync.init(files, {
         proxy: 'http://localhost:8888/'
     })
-  });
+});
+
+gulp.task('sass-lint', function () {
+    return gulp.src([
+        'assets/css/**/*.scss'
+        //'!sass/dont-watch-this.scss'
+    ])
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
