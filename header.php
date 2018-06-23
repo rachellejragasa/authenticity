f9f8c5860df15ef87236a6c260b0a627d5d06cf3
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-06-23 11:12:14 +0100

    Updating content

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Stylish Portfolio - Start Bootstrap Template</title>
+    <title>Authenticity Dance Company</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css'; ?>" rel="stylesheet">
@@ -35,11 +35,11 @@
     <!-- Header -->
     <header class="masthead d-flex">
       <div class="container text-center my-auto">
-        <h1 class="mb-1">Stylish Portfolio</h1>
+        <!-- <h1 class="mb-1">Stylish Portfolio</h1>
         <h3 class="mb-5">
           <em>A Free Bootstrap Theme by Start Bootstrap</em>
         </h3>
-        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a> -->
       </div>
       <div class="overlay"></div>
     </header>
--- index.php
+++ index.php
@@ -1,168 +1,219 @@
 <?php get_header(); ?>  
 
- <!-- About the Company -->
- <section class="content-section bg-light" id="about-the-company">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-lg-10 mx-auto">
-          <div class="content-section-heading">
-            <h2 class="mb-5">About the Company</h2>
-          </div>
-            <p class="lead mb-5">
-            Authenticity is a brand new exciting company for talented young authentic dancers aged 11-
-            18 to represent London. The aim is to highlight the elite dance talent within London, provide
-            performance opportunities that are accessible and affordable as well as nurture a safe and
-            creative environment to train and grow within dance.
-            For their opening works, dancers are working alongside the Impact Dance creative team to
-            create a 15-minute piece of work, which will be performed outdoors at this year’s ‘Dancing
-            City’(Greenwich Docklands International Festival) and ‘Imagine Watford’ (Watford Palace
-            Theatre) at the end of June/beginning of July 2018. 
-            </p>
-          </div>
-        </div>
+<!-- About the Company -->
+<section class="content-section bg-light" id="about-the-company">
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-lg-10 mx-auto">
+      <div class="content-section-heading">
+        <h2 class="mb-5">About the Company</h2>
       </div>
-    </section>
-
-    <!-- About the Team -->
-    <section class="content-section bg-primary text-white text-center" id="about-the-team">
-      <div class="container">
-        <div class="content-section-heading">
-          <h2 class="mb-5">About the Team</h2>
-        </div>
-        <div class="row">
-          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-            <span class="service-icon rounded-circle mx-auto mb-3">
-              <i class="icon-screen-smartphone"></i>
-            </span>
-            <h4>
-              <strong>Executive Director Hakeem Onibudo</strong>
-            </h4>
-            <p class="text-faded mb-0">
-            Impact Dance founder, British Council Ambassador, Movie Premiere Host – Suicide Squad,
-            Star Wars The Last Jedi, The Force Awakens and Fast and Furious 6 to name a few,
-            Breakin’ Convention, LIVEVIBE and most recently Associate Producer at The Place on the
-            Arts Council funded Changemakers programme.
-            </p>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-            <span class="service-icon rounded-circle mx-auto mb-3">
-              <i class="icon-pencil"></i>
-            </span>
-            <h4>
-              <strong>Team Member 2</strong>
-            </h4>
-            <p class="text-faded mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
-            <span class="service-icon rounded-circle mx-auto mb-3">
-              <i class="icon-like"></i>
-            </span>
-            <h4>
-              <strong>Team Member 3</strong>
-            </h4>
-            <p class="text-faded mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <span class="service-icon rounded-circle mx-auto mb-3">
-              <i class="icon-mustache"></i>
-            </span>
-            <h4>
-              <strong>Team Member 4</strong>
-            </h4>
-            <p class="text-faded mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-        </div>
+        <p class="lead mb-5">
+        Authenticity is a brand new exciting company for talented young authentic dancers aged 11-
+        18 to represent London. The aim is to highlight the elite dance talent within London, provide
+        performance opportunities that are accessible and affordable as well as nurture a safe and
+        creative environment to train and grow within dance.
+        For their opening works, dancers are working alongside the Impact Dance creative team to
+        create a 15-minute piece of work, which will be performed outdoors at this year’s ‘Dancing
+        City’(Greenwich Docklands International Festival) and ‘Imagine Watford’ (Watford Palace
+        Theatre) at the end of June/beginning of July 2018. 
+        </p>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
 
-    <!-- Authenticity Inaugural Company -->
-    <section class="content-section bg-light" id="company">
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-lg-10 mx-auto">
-          <div class="content-section-heading">
-            <h2 class="mb-5">Authenticity Inaugural Company</h2>
-          </div>
-            <p class="lead mb-5">
-            We have dancers from the following London Boroughs: 
-            <br>
-            Kensington & Chelsea
-            <br>
-            Southwark
-            <br>
-            Lambeth
-            <br>
-            Bexley
-            <br>
-            Croydon
-            <br>
-            Hackney
-            <br>
-            Newham
-            <br>
-            Greenwich
-            <br>
-            Brent
-            </p>
-          </div>
-        </div>
+<!-- About the Team -->
+<section class="content-section bg-primary text-white text-center" id="about-the-team">
+  <div class="container">
+    <div class="content-section-heading">
+      <h2 class="mb-5">About the Team</h2>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+        <span class="service-icon rounded-circle mx-auto mb-3">
+          <i class="icon-screen-smartphone"></i>
+        </span>
+        <h4>
+          <strong>Hakeem Onibudo</strong>
+        </h4>
+        <h5>Executive Director </h5>
+        <p class="text-faded mb-0">
+        Impact Dance founder, British Council Ambassador, Movie Premiere Host – Suicide Squad,
+        Star Wars The Last Jedi, The Force Awakens and Fast and Furious 6 to name a few,
+        Breakin’ Convention, LIVEVIBE and most recently Associate Producer at The Place on the
+        Arts Council funded Changemakers programme.
+        </p>
       </div>
-    </section>
-
-    <!-- Portfolio -->
-    <section class="content-section" id="portfolio">
-      <div class="container">
-        <div class="content-section-heading text-center">
-          <h3 class="text-secondary mb-0">Portfolio</h3>
-          <h2 class="mb-5">Recent Projects</h2>
-        </div>
-        <div class="row no-gutters">
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
-                  <h2>Stationary</h2>
-                  <p class="mb-0">A yellow pencil with envelopes on a clean, blue backdrop!</p>
-                </span>
-              </span>
-              <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-1.jpg'; ?>" alt="">
-            </a>
-          </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
-                  <h2>Ice Cream</h2>
-                  <p class="mb-0">A dark blue background with a colored pencil, a clip, and a tiny ice cream cone!</p>
-                </span>
-              </span>
-              <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-2.jpg'; ?>" alt="">
-            </a>
-          </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
-                  <h2>Strawberries</h2>
-                  <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
-                </span>
-              </span>
-              <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-3.jpg'; ?>" alt="">
-            </a>
-          </div>
-          <div class="col-lg-6">
-            <a class="portfolio-item" href="#">
-              <span class="caption">
-                <span class="caption-content">
-                  <h2>Workspace</h2>
-                  <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
-                </span>
-              </span>
-              <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-4.jpg'; ?>" alt="">
-            </a>
-          </div>
-        </div>
+      <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+        <span class="service-icon rounded-circle mx-auto mb-3">
+          <i class="icon-pencil"></i>
+        </span>
+        <h4>
+          <strong>Paige Jarrett</strong>
+        </h4>
+        <h5>Operations Manager</h5>
+        <p class="text-faded mb-0">Paige Jarrett is a graduate from Kingston University and has 
+        been working alongside Onibudo as the Trainee Producer at The Place on the Arts Council 
+        Funded Changemakers Programme for the last eight months September 2017 - April 2018. 
+        Paige has also worked on Ignition Dance Festival produced by DanceWest (2016 - 2018). 
+        During her role at the Place, Paige was responsible for producing 3 major events including 
+        LIVEVIBE, MIXIN’ MEDIA and TUNE-D IN. Paige is now a on her way to establishing herself as 
+        one of the new leaders in the arts and can easily fill the role of any creative producer 
+        within any  high level arts organisation.</p>
       </div>
-    </section>
+      <div class="col-lg-4 col-md-6 mb-5 mb-md-0">
+        <span class="service-icon rounded-circle mx-auto mb-3">
+          <i class="icon-like"></i>
+        </span>
+        <h4>
+          <strong>Rebecca Leslie</strong>
+        </h4>
+        <h5>Participation Manager</h5>
+        <p class="text-faded mb-0">Rebecca Leslie trained at London Studio Centre. After graduating 
+        she freelanced as an arts administrative in organisations who specialise working with young 
+        people such as Wac Arts, Step Into Dance and Community Focus. Rebecca went on to apply for 
+        the role of Impact Youth Academy manager. On successfully being appointed with this organisation, 
+        Rebecca has been coordinating the youth academy for the past 6 months exhibiting strong 
+        organizational skills and efficiency in running the academy. Her knowledge of young people and 
+        expertise in liaising with parents puts her in as strong position to support this project as a
+        participation manager.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
-    <?php get_footer(); ?>
+<!-- Authenticity Inaugural Company -->
+<section class="content-section bg-light" id="company">
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-lg-10 mx-auto">
+      <div class="content-section-heading">
+        <h2 class="mb-5">Authenticity Inaugural Company</h2>
+      </div>
+        <p class="lead mb-5">
+        We have dancers from the following London Boroughs: 
+        <br>
+        Kensington & Chelsea
+        <br>
+        Southwark
+        <br>
+        Lambeth
+        <br>
+        Bexley
+        <br>
+        Croydon
+        <br>
+        Hackney
+        <br>
+        Newham
+        <br>
+        Greenwich
+        <br>
+        Brent
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Current Work -->
+<section class="content-section bg-primary text-white text-center" id="current-work">
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-lg-10 mx-auto">
+      <div class="content-section-heading">
+        <h2 class="mb-5">Current Work</h2>
+      </div>
+        <p class="lead mb-5">
+        Authenticity is a brand new exciting company for talented young authentic dancers aged 11-
+        18 to represent London. The aim is to highlight the elite dance talent within London, provide
+        performance opportunities that are accessible and affordable as well as nurture a safe and
+        creative environment to train and grow within dance.
+        For their opening works, dancers are working alongside the Impact Dance creative team to
+        create a 15-minute piece of work, which will be performed outdoors at this year’s ‘Dancing
+        City’(Greenwich Docklands International Festival) and ‘Imagine Watford’ (Watford Palace
+        Theatre) at the end of June/beginning of July 2018. 
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Portfolio -->
+<section class="content-section" id="portfolio">
+  <div class="container">
+    <div class="content-section-heading text-center">
+      <h3 class="text-secondary mb-0">Portfolio</h3>
+      <h2 class="mb-5">Dancers</h2>
+    </div>
+    <div class="row no-gutters">
+      <div class="col-lg-6">
+        <a class="portfolio-item" href="#">
+          <span class="caption">
+            <span class="caption-content">
+              <h2>Stationary</h2>
+              <p class="mb-0">A yellow pencil with envelopes on a clean, blue backdrop!</p>
+            </span>
+          </span>
+          <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-1.jpg'; ?>" alt="">
+        </a>
+      </div>
+      <div class="col-lg-6">
+        <a class="portfolio-item" href="#">
+          <span class="caption">
+            <span class="caption-content">
+              <h2>Ice Cream</h2>
+              <p class="mb-0">A dark blue background with a colored pencil, a clip, and a tiny ice cream cone!</p>
+            </span>
+          </span>
+          <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-2.jpg'; ?>" alt="">
+        </a>
+      </div>
+      <div class="col-lg-6">
+        <a class="portfolio-item" href="#">
+          <span class="caption">
+            <span class="caption-content">
+              <h2>Strawberries</h2>
+              <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
+            </span>
+          </span>
+          <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-3.jpg'; ?>" alt="">
+        </a>
+      </div>
+      <div class="col-lg-6">
+        <a class="portfolio-item" href="#">
+          <span class="caption">
+            <span class="caption-content">
+              <h2>Workspace</h2>
+              <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
+            </span>
+          </span>
+          <img class="img-fluid" src="<?php echo get_template_directory_uri() . '/img/portfolio-4.jpg'; ?>" alt="">
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
